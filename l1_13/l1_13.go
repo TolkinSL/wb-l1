356de88761954c964e550b7e3462743760085f47
TolkinSL <tolkinsl@yandex.ru>
2025-11-13 05:04:41 +0300

    dev13 - Решение задачи L1.13

---
+++ l1_13/l1_13.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+	b := 5
+	fmt.Printf("a = %d, b = %d\n", a, b)
+
+	a = a + b
+	b = a - b
+	a = a - b
+	fmt.Printf("a = %d, b = %d\n", a, b)
+}
