efa92732ab35b4c85e9a443a4e13a43f26963705
TolkinSL <tolkinsl@yandex.ru>
2025-11-18 09:16:42 +0300

    dev14 - Решение задачи L1.14

---
+++ l1_14/l1_14.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// any — это синоним interface{}
+
+func myType(v any) {
+    switch v.(type) {
+    case int:
+        fmt.Println("Тип: int")
+    case string:
+        fmt.Println("Тип: string")
+    case bool:
+        fmt.Println("Тип: bool")
+    case chan int, chan string, chan bool, chan any:
+        fmt.Println("Тип: chan")
+    default:
+        fmt.Println("Тип: unknown type")
+    }
+}
+
+func main() {
+    myType(7)
+    myType("Go")
+    myType(false)
+
+    ch := make(chan int)
+    myType(ch)
+
+		myType(3.14)
+}
+
